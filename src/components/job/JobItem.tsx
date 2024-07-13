43622a3e838ea998e2f2af29db0991de8dfba989
miawithcode <cuttontail@gmail.com>
2024-07-13 20:29:29 +0800

    feat: write active job id to url

--- src/components/job/JobItem.tsx
+++ src/components/job/JobItem.tsx
@@ -7,20 +7,22 @@ type JobItemProps = {
 
 const JobItem = ({ job }: JobItemProps) => {
   return (
-    <li className="flex w-full cursor-pointer items-start gap-3 border-b px-2 py-3 transition last:border-b-0 hover:bg-slate-100">
-      <div className="grid h-10 w-10 place-content-center rounded-[4px] border border-black font-semibold">
-        {job.badgeLetters}
-      </div>
-      <div className="flex-1">
-        <h3 className="font-semibold">{job.title}</h3>
-        <p className="text-sm tracking-tight">{job.company}</p>
-        <p className="text-sm text-slate-400">{job.daysAgo}d</p>
-      </div>
-      <div>
-        <button>
-          <BookmarkIcon />
-        </button>
-      </div>
+    <li className="w-full cursor-pointer gap-3 border-b px-2 py-3 transition last:border-b-0 hover:bg-slate-100">
+      <a href={`#${job.id}`} className="flex w-full items-start gap-3">
+        <div className="grid h-10 w-10 place-content-center rounded-[4px] border border-black font-semibold">
+          {job.badgeLetters}
+        </div>
+        <div className="flex-1">
+          <h3 className="font-semibold">{job.title}</h3>
+          <p className="text-sm tracking-tight">{job.company}</p>
+          <p className="text-sm text-slate-400">{job.daysAgo}d</p>
+        </div>
+        <div>
+          <button>
+            <BookmarkIcon />
+          </button>
+        </div>
+      </a>
     </li>
   );
 };
