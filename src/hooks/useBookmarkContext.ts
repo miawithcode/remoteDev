2549697db744cf001b09bd60ff8029197a7acfa3
miawithcode <cuttontail@gmail.com>
2024-07-16 18:05:48 +0800

    chore: update useContext error message

--- src/hooks/useBookmarkContext.ts
+++ src/hooks/useBookmarkContext.ts
@@ -4,7 +4,9 @@ import { BookmarkContext } from "../contexts/BookmarkContextProvider";
 const useBookmarkContext = () => {
   const context = useContext(BookmarkContext);
   if (!context) {
-    throw new Error("BookmarkContext is not defined");
+    throw new Error(
+      "useBookmarkContext must be used within a BookmarkContextProvider",
+    );
   }
   return context;
 };
