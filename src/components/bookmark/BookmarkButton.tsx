66e21bedb7543c5100869ffd459287caacf19218
miawithcode <cuttontail@gmail.com>
2024-07-17 15:53:09 +0800

    feat: close popover on click outside

--- src/components/bookmark/BookmarkButton.tsx
+++ src/components/bookmark/BookmarkButton.tsx
@@ -1,19 +1,25 @@
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import BookmarkPopover from "./BookmarkPopover";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 const BookmarkButton = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
+  useOnClickOutside([buttonRef, popoverRef], () => setIsOpen(false));
+
   return (
     <div className="relative">
       <button
+        ref={buttonRef}
         onClick={() => setIsOpen((prev) => !prev)}
         className="flex items-center gap-2 rounded border border-black px-2 py-1"
       >
         Saved jobs <ChevronDownIcon />
       </button>
-      {isOpen && <BookmarkPopover />}
+      {isOpen && <BookmarkPopover ref={popoverRef} />}
     </div>
   );
 };
---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+const useOnClickOutside = (
+  refs: React.RefObject<HTMLElement>[],
+  handler: () => void,
+) => {
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (refs.every((ref) => !ref.current?.contains(e.target as Node))) {
+        handler();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => document.removeEventListener("click", handleClick);
+  }, [refs, handler]);
+};
+export default useOnClickOutside;
